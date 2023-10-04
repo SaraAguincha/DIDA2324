ca73ac09735abc11ddec227371ab27c255d118d7
Sara Aguincha <sara.aguincha@tecnico.ulisboa.pt>
2023-10-04 01:09:06 +0100

    minor additions

--- LServer/Services/LServerService.cs
+++ LServer/Services/LServerService.cs
@@ -1,4 +1,5 @@
-﻿using Protos;
+﻿using Grpc.Net.Client;
+using Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,28 @@ namespace LServer.Services
     {
         // Server attributes
         private string lManagerID;
+        private int processId;      // needed in order to select the leader
+
+
+        private Dictionary<string, GrpcChannel> channels = new Dictionary<string, GrpcChannel>();
+        private Dictionary<string, PaxosService.PaxosServiceClient> lServerInstances = new Dictionary<string, PaxosService.PaxosServiceClient>();
+        private Dictionary<string, string> LServers;
+
 
         // Lease request queue for this Lease Manager
         Queue<GrantLeaseRequest> leaseRequestQueue = new Queue<GrantLeaseRequest>();
-        public LServerService(string lManagerID) 
+        public LServerService(string lManagerID, Dictionary<string, string> lServers) 
         {
             this.lManagerID = lManagerID;
+            this.LServers = lServers;
+
+            // populate all of lservers connections
+            foreach (KeyValuePair<string, string> lserver in this.LServers)
+            {
+                GrpcChannel channel = GrpcChannel.ForAddress(lserver.Value);
+                channels.Add(lserver.Key, channel);
+                lServerInstances.Add(lserver.Key, new PaxosService.PaxosServiceClient(channel));
+            }
         }
 
         public GrantLeaseReply GrantLease(GrantLeaseRequest request)
@@ -41,6 +58,30 @@ namespace LServer.Services
 
             return leaseReply;
         }
+
+        /*  Paxos: Prepare
+         *  Sends a prepare request to all the other servers
+         */
+        public PromiseReply Prepare(PrepareRequest request)
+        {
+            List<PromiseReply> promisesReplies = new List<PromiseReply>();
+            List<Task> tasks = new List<Task>();
+            // for each LServer starts a task with a prepare request
+            foreach (KeyValuePair<string, PaxosService.PaxosServiceClient> lServerInstances in this.lServerInstances)
+            {
+                // for each entry in LServers, run a task with the request for a lease
+                Task t = Task.Run(() =>
+                {
+                    lServerInstances.Value.PrepareAsync(request);
+                });
+                tasks.Add(t);
+            }
+
+            PromiseReply reply = new PromiseReply();
+            return reply;
+        }
+
+
 
     }
 }
